2ab21201129337476a42b7e0ffe37d6317ddd5e6
Emily Hill <emhillmed2021@gmail.com>
2022-12-23 00:21:27 -0600

    final push, the old repo

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,15 +3,18 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    butInstall.classList.toggle('hidden', false);
+    event.preventDefault();
+    butInstall.style.visibility = 'visible';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    butInstall.classList.toggle('hidden', true); 
+    event.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!'; 
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    
+    console.log('App installed!', event)
 });
